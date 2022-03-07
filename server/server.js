3a05a154db3fd7a53c34637a6161eb0004f92566
leeshaSamadhi <leesha.18@itfac.mrt.ac.lk>
2022-03-07 23:34:26 +0530

    Added the UI and create-delete functionalities

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
+const Todo = require('./models/Todo');
+
 
 app.use(express.json());
 app.use(cors());
@@ -11,19 +13,36 @@ mongoose.connect('mongodb://127.0.0.1:27017/todo', {
     useUnifiedTopology: true
 }).then(() => console.log("Connected to database")).catch(console.error);
 
+//routes
 
-// Models
-const Todo = require('./models/Todo');
 app.get('/todos', async (req, res) => {
     const todos = await Todo.find();
     res.json(todos);
 });
+
 app.post('/todo/new', (req, res) => {
     const todo = new Todo({
         text: req.body.text
-    })
+    });
     todo.save();
     res.json(todo);
 });
+
+app.delete('/todo/delete/:id', async (req, res) => {
+    const result = await Todo.findByIdAndDelete(req.params.id);
+    res.json({result});
+});
+
+app.get('/todo/removed/:id', async (req, res) => {
+    const result = await Todo.findById(req.params.id);
+    res.json({result});
+});
+
+app.get('/todo/complete/:id', async(req, res)=>{
+    const todo = await Todo.findById(req.params.id);
+    todo.complete = !todo.complete;
+    todo.save();
+    res.json(todo);
+})
 
 app.listen(3001, ()=> console.log(`server started on port 3001`));
